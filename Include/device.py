131a678281df3d680b8705f1093ff968e84eb29c
Joost Blaawwiekel <keeperjoost@gmail.com>
2019-11-22 15:56:15 +0100

    made a lot of changes

---
+++ Include/device.py
@@ -0,0 +1,60 @@
+import serial as serial
+import time
+import threading
+from threading import *
+
+
+# noinspection PyMethodMayBeStatic
+class Device(threading.Thread):
+
+    def __init__(self, device, ser):
+        # Thread.__init__(self)
+        threading.Thread.__init__(self)
+        self.ser = ser
+        self.device = device
+        self.last_measure = 0
+
+    # run thread function for Device object
+    def run(self):
+        time.sleep(1)
+        self.last_measure = 0
+        self.main()
+
+    # function that write a number to the arduino
+    def write_data(self, data):  # something like b'\x02'
+        self.ser.write(data)
+        time.sleep(.5)
+
+    # function that returns a number from the arduino
+    def read_data(self):
+        line = self.ser.read()
+        # only show bytes with content, not the empty ones
+        if line != b'':
+            while line is not None:
+                # make sure to use int.from_bytes(line, "big")
+                # otherwise it will print something like b'\xa4'
+
+
+                   return ord(line)
+
+    # main function
+    def main(self):
+
+        data = self.read_data()
+        if data is not None:
+            self.last_measure = data
+
+    # getter for last measurement
+    def get_last_measure(self):
+        return self.last_measure
+
+    # getter for device name
+    def get_device(self):
+        return self.device
+
+    # function that calls a command from the arduino
+    def send_command(self, data):
+
+        # send command value to arduino and wait for a response
+        self.write_data(data)
+        print("Command send")
---
+++ Include/port.py
@@ -0,0 +1,81 @@
+import serial
+import serial.tools.list_ports
+import threading
+from Include.device import Device
+from threading import *
+
+
+# noinspection PyBroadException,PyArgumentList
+class SerialThread(threading.Thread):
+
+    # initialization of a SerialThread object
+    # here is dictionary with as index the COM port and
+    # the value the type of device, like 'TEMPERATURE'
+    def __init__(self):
+        # Thread.__init__(self)
+        threading.Thread.__init__(self)
+        self.connected_devices = {}
+
+    # function that runs updates on the ports
+    def run(self):
+        self.update()
+
+    # function to update port states
+    def update(self):
+        self.scan_ports()
+        threading.Timer(1, self.update).start()
+
+    def read_data(self):
+        line = self.ser.read()
+        # only show bytes with content, not the empty ones
+        if line != b'':
+            while line is not None:
+                # make sure to use int.from_bytes(line, "big")
+                # otherwise it will print something like b'\xa4'
+
+                return ord(line)
+
+    def handshake(self):
+        if self.read_data() == 255:
+            self.write_data(b'\xFF')
+            device_id = self.read_data()
+
+            if device_id == 150:  # 0x96
+                device = 'TEMPERATURE'
+            elif device_id == 105:  # 0x69
+                device = 'LIGHT'
+            else:
+                print("ERROR: Something went wrong with the handshake!")
+
+            print('Unit is:', device)
+
+            return device
+
+    # this function scans ports to see if a device is connected
+    def scan_ports(self):
+        for port in serial.tools.list_ports.comports():
+            try:
+                # if a device is not connected and found by this function:
+                if port not in self.connected_devices:
+                    print('Found device on:', port.device)
+
+                    # found a device, now perform the handshake
+                    device = self.handshake()
+
+                    # now we know what type device we have
+                    # create a device object and save it in the
+                    # self.connected devices dictionary
+                    ser = serial.Serial(port, baudrate=19200, timeout=5)
+                    temp = Device(device, ser)
+                    self.connected_devices[port] = temp
+                    print(device, "Connected")
+
+                # when a the list has a port with no device left
+                # delete the key and value from the dictionary
+                # THIS PART HAS NOT BEEN TESTED YET
+                for key_port, value_device in self.connected_devices.items():
+                    if port != key_port:
+                        print(value_device, "disconnected from port", key_port)
+                        del self.connected_devices[key_port]
+            except:
+                continue
