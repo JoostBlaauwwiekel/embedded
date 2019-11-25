0c34744232072852cc8ca9b98c81da082939c3f5
Joost Blaawwiekel <keeperjoost@gmail.com>
2019-11-25 19:40:33 +0100

    gui works so far

--- Include/gui.py
+++ Include/gui.py
@@ -9,7 +9,7 @@ class Gui(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
-        self.last_temperature = "Onbekend "
+        self.last_temperature = "Onbekend"
         self.last_light = "Onbekend"
         self.last_wind = "Onbekend"
         self.last_rain = "Onbekend"
@@ -36,17 +36,23 @@ class Gui(threading.Thread):
     def set_devices(self, devices):
         self.connected_devices = devices
 
+    def set_light(self, light):
+        self.last_light = light
+
+    def set_temperature(self, temperature):
+        self.last_temperature = temperature
+
     def get_last_temperature(self):
         if self.last_temperature == 'Onbekend':
             return ''
         else:
-            return str(self.last_temperature) + "°C"
+            return "\n" + str(self.last_temperature) + "°C"
 
     def get_last_light(self):
         if self.last_light == 'Onbekend':
             return ''
         else:
-            return str(self.last_light) + " Lumen"
+            return "\n" + str(self.last_light) + " Lumen"
 
     def get_last_wind(self):
         if self.last_wind == 'Onbekend':
@@ -64,50 +70,84 @@ class Gui(threading.Thread):
         if self.last_air == 'Onbekend':
             return ''
         else:
-            return str(self.last_air) + "&"
+            return str(self.last_air) + "%"
 
     def change_light(self):
-        if "LIGHT" in self.connected_devices:
-            return "Lichtsenor\n\nAangesloten"
+        for device in self.connected_devices:
+            if device[0] == "LIGHT":
+                return "Lichtsenor\n\nAangesloten"
+            else:
+                return "Lichtsenor\n\nNiet aangesloten"
+
+        return "Lichtsenor\n\nNiet aangesloten"
 
     def change_temperature(self):
-        if "TEMPERATURE" in self.connected_devices:
-            return "Temperatuursensor\n\nAangesloten"
+        for device in self.connected_devices:
+            if device[0] == "TEMPERATURE":
+                return "Temperatuursensor\n\nAangesloten"
+            else:
+                return "Temperatuursensor\n\nNiet aangesloten"
+
+        return "Temperatuursensor\n\nNiet aangesloten"
 
     def change_wind(self):
-        if "WIND" in self.connected_devices:
-            return "Anemometer\n\nAangesloten"
+        for device in self.connected_devices:
+            if device[0] == "WIND":
+                return "Anemometer\n\nAangesloten"
+
+        return "Anemometer\n\nNiet aangesloten"
 
     def change_rain(self):
-        if "RAIN" in self.connected_devices:
-            return "Regensensor\n\nAangesloten"
+        for device in self.connected_devices:
+            if device[0] == "RAIN":
+                return "Regensensor\n\nAangesloten"
+
+        return "Regensensor\n\nNiet aangesloten"
 
     def change_air(self):
-        if "AIR" in self.connected_devices:
-            return "Luchtvochtigheidsensor\n\nAangesloten"
+        for device in self.connected_devices:
+            if device[0] == "AIR":
+                return "Luchtvochtigheidsensor\n\nAangesloten"
+
+        return "Luchtvochtigheidsensor\n\nNiet aangesloten"
 
     def update(self):
-        if "LIGHT" in self.connected_devices:
+        self.change_gui()
+        self.detect_disconnected_devices()
+
+        threading.Timer(1, self.update).start()
+
+    def change_gui(self):
+        # change values in the GUI
+        print(self.connected_devices)
+        try:
             self.label1.config(text=self.change_light())
             self.light.config(text=self.get_last_light())
 
-        if "TEMPERATURE" in self.connected_devices:
             self.label2.config(text=self.change_temperature())
             self.temperature.config(text=self.get_last_temperature())
 
-        if "WIND" in self.connected_devices:
             self.label3.config(text=self.change_wind())
             self.wind.config(text=self.get_last_wind())
 
-        if "RAIN" in self.connected_devices:
             self.label4.config(text=self.change_rain())
             self.rain.config(text=self.get_last_rain())
 
-        if "AIR" in self.connected_devices:
             self.label5.config(text=self.change_air())
             self.air.config(text=self.get_last_air())
+        except: TypeError
 
-        threading.Timer(1, self.update).start()
+    def detect_disconnected_devices(self):
+        if not self.connected_devices:
+            self.last_temperature = "Onbekend"
+            self.last_light = "Onbekend"
+            self.last_air = "Onbekend"
+            self.last_rain = "Onbekend"
+            self.last_wind = "Onbekend"
+
+        for device in self.connected_devices:
+            if self.last_temperature != "Onbekend" and device[0] != 'TEMPERATURE':
+                self.last_temperature = "Onbekend"
 
     def render(self):
         root = Tk()
@@ -134,24 +174,24 @@ class Gui(threading.Thread):
         tab6 = ttk.Label(tabControl)
         tabControl.add(tab6, text="Luchtvochtigheid")
 
-        root.columnconfigure(0, weight = 1)
+        root.columnconfigure(0, weight=1)
         root.columnconfigure(1, weight=1)
         root.columnconfigure(2, weight=1)
         root.columnconfigure(3, weight=1)
         root.columnconfigure(4, weight=1)
 
-        self.label1 = ttk.Label(tab1, text="Lichtsenor\n\nNiet aangesloten", width = 20, anchor = S)
+        self.label1 = ttk.Label(tab1, text=self.change_light(), width=20, anchor=S, justify=CENTER)
         self.label1.config(text=self.change_light())
-        self.label2 = ttk.Label(tab1, text="Temperatuursensor\n\nNiet aangesloten", width = 20, anchor = S)
+        self.label2 = ttk.Label(tab1, text=self.change_temperature(), width=20, anchor=S, justify=CENTER)
         self.label2.config(text=self.change_temperature())
-        self.label3 = ttk.Label(tab1, text="Anemometer\n\nNiet aangesloten", width = 20, anchor = S)
+        self.label3 = ttk.Label(tab1, text=self.change_wind(), width=20, anchor=S, justify=CENTER)
         self.label3.config(text=self.change_wind())
-        self.label4 = ttk.Label(tab1, text="Regensensor\n\nNiet aangesloten", width = 20, anchor = S)
+        self.label4 = ttk.Label(tab1, text=self.change_rain(), width=20, anchor=S, justify=CENTER)
         self.label4.config(text=self.change_rain())
-        self.label5 = ttk.Label(tab1, text="Luchtvochtigheid\n\nNiet aangesloten", width = 20, anchor = S)
+        self.label5 = ttk.Label(tab1, text=self.change_air(), width=20, anchor=S, justify=CENTER)
         self.label5.config(text=self.change_air())
 
-        self.light = ttk.Label(tab1, text = "\n\n" + self.get_last_light(), anchor = S, font = ('arial', 18))
+        self.light = ttk.Label(tab1, text="\n\n" + self.get_last_light(), anchor=S, font=('arial', 18))
         self.light.config(text=self.get_last_light())
         self.temperature = ttk.Label(tab1, text="\n\n" + self.get_last_temperature(), anchor=S, font=('arial', 18))
         self.temperature.config(text=self.get_last_temperature())
@@ -162,19 +202,19 @@ class Gui(threading.Thread):
         self.air = ttk.Label(tab1, text="\n\n" + self.get_last_air(), anchor=S, font=('arial', 18))
         self.air.config(text=self.get_last_air())
 
-        self.label1.grid(row = 1, column = 0, stick = "nsew", ipady = 20)
-        self.label2.grid(row = 1, column = 1, stick = "nsew", ipady = 20)
-        self.label3.grid(row = 1, column = 2, stick = "nsew", ipady = 20)
-        self.label4.grid(row = 1, column = 3, stick = "nsew", ipady = 20)
-        self.label5.grid(row = 1, column = 4, stick = "nsew", ipady = 20)
+        self.label1.grid(row=1, column=0, stick="nsew", ipady=20)
+        self.label2.grid(row=1, column=1, stick="nsew", ipady=20)
+        self.label3.grid(row=1, column=2, stick="nsew", ipady=20)
+        self.label4.grid(row=1, column=3, stick="nsew", ipady=20)
+        self.label5.grid(row=1, column=4, stick="nsew", ipady=20)
 
-        self.light.grid(row = 0, column = 0, stick = "nsew")
+        self.light.grid(row=0, column=0, stick="nsew")
         self.temperature.grid(row=0, column=1, stick="nsew")
         self.wind.grid(row=0, column=2, stick="nsew")
         self.rain.grid(row=0, column=3, stick="nsew")
         self.air.grid(row=0, column=4, stick="nsew")
 
         # just because its possible
-        ttk.Label(tab1, text="\nCopyright © 2019 Zeng Ltd.").grid(row = 2, columnspan = 5)
+        ttk.Label(tab1, text="\nCopyright © 2019 Zeng Ltd.").grid(row=2, columnspan=5)
 
         root.mainloop()
--- Include/main.py
+++ Include/main.py
@@ -19,15 +19,18 @@ class Main(Thread):
 
         while(True):
             connected_devices = self.serial.get_connected_devices()
+            self.gui.set_devices(connected_devices)
             sleep(5)
 
             try:
                 for device in connected_devices:
                     if device[0] == 'TEMPERATURE':
                         last_temperature = device[1].read_data()
+                        self.gui.set_temperature(last_temperature)
 
                     if device[0] == 'LIGHT':
                         last_light = device[1].read_data()
+                        self.gui.set_light(last_light)
             except:
                 continue
 
