d36f4c81fa7c609ab7c51c4c4ffa55a5bf0b3f04
Hessel Jager <hessel_fj@hotmail.com>
2019-11-26 12:38:48 +0100

    functies voor alle nodige commands gemaakt en benodigde variabelen toegevoegd

--- Include/device.py
+++ Include/device.py
@@ -12,6 +12,9 @@ class Device(threading.Thread):
         self.ser = ser
         self.device = device
         self.last_measure = 0
+        self.device_min_border = 10
+        self.device_max_border = 50
+        self.device_auto_roll = True
 
     # run thread function for Device object
     def run(self):
@@ -32,9 +35,7 @@ class Device(threading.Thread):
             while line is not None:
                 # make sure to use int.from_bytes(line, "big")
                 # otherwise it will print something like b'\xa4'
-
-
-                   return ord(line)
+                return ord(line)
 
     # main function
     def main(self):
@@ -51,9 +52,52 @@ class Device(threading.Thread):
     def get_device(self):
         return self.device
 
-    # function that calls a command from the arduino
-    def send_command(self, data):
+    # change maximum roll out border
+    def change_max_border(self, new_max):
+        if(new_max > self.device_min_border):
+            self.write_data(b'\x01')
+            time.sleep(1)
+            # self.write_data() -> hier moet new_max als byte getal gestuurd worden
+            self.device_max_border = new_max
+            print("Maximale uitrol waarde is succesvol aangepast")
+        else:
+            print("Fout: Maximale uitrol waarde mag niet kleiner zijn dan de maximale oprol waarde")
 
-        # send command value to arduino and wait for a response
-        self.write_data(data)
-        print("Command send")
+    # change maximum roll in border
+    def change_min_border(self, new_min):
+        if(new_min < self.device_max_border):
+            self.write_data(b'\x02')
+            time.sleep(1)
+            # self.write_data() -> hier moet new_min als byte getal gestuurd worden
+            self.device_min_border = new_min
+            print("Maximale oprol waarde is succesvol aangepast")
+        else:
+            print("Fout: Maximale oprol waarde mag niet groter zijn dan de maximale uitrol waarde")
+
+    # manually roll out
+    def manual_roll_out(self):
+        self.write_data(b'\x03')
+        print("Het rolluik rolt nu uit (let op: automatisch rollen is uitgeschakeld)")
+
+    # manually roll in
+    def manual_roll_in(self):
+        self.write_data(b'\x04')
+        print("Het rolluik rolt nu op (let op: automatisch rollen is uitgeschakeld)")
+
+    # reset border and auto roll values back to default
+    def reset_to_default(self):
+        self.write_data(b'\x05')
+        self.device_min_border = 10
+        self.device_max_border = 50
+        self.device_auto_roll = True
+        print("De maximale op- en uitrol waarden zijn gereset naar de standaard waarden")
+
+    # disable automatic rolling
+    def disable_auto_roll(self):
+        self.write_data(b'\x06')
+        print("Automatisch rollen is nu uitgeschakeld")
+
+    # enable automatic rolling
+    def enable_auto_roll(self):
+        self.write_data(b'\x07')
+        print("Automatisch rollen is nu ingeschakeld")
